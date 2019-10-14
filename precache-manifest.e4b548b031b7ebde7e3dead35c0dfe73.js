6915cb27b90dc18d8ed8f6c6bdc13f2feaa24100
Damius Green <40849814+Iforgedit@users.noreply.github.com>
2019-10-14 15:37:10 -0400

    Updates

---
+++ precache-manifest.e4b548b031b7ebde7e3dead35c0dfe73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b27ab711fb0fc38c989bc2407ee38617",
+    "url": "/microwaves/index.html"
+  },
+  {
+    "revision": "f98e31710664e63ce93e",
+    "url": "/microwaves/static/css/main.6587be9e.chunk.css"
+  },
+  {
+    "revision": "300a5debdc976e5bf713",
+    "url": "/microwaves/static/js/2.25b1c4fe.chunk.js"
+  },
+  {
+    "revision": "f98e31710664e63ce93e",
+    "url": "/microwaves/static/js/main.0506719e.chunk.js"
+  },
+  {
+    "revision": "a6fdb2b98c63ff3ec0b7",
+    "url": "/microwaves/static/js/runtime~main.9a6a6ba0.js"
+  }
+]);
